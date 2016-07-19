7031752af119b1e8e44e2c4f472519fc97ac3bde
SongFuZhen <evil_song@126.com>
2016-07-19 17:42:27 +0800

    kpi settings

--- db/schema.rb
+++ db/schema.rb
@@ -117,6 +117,9 @@ ActiveRecord::Schema.define(version: 20160719025752) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_project_items_on_project_id", using: :btree
+    t.index ["rank"], name: "index_project_items_on_rank", using: :btree
+    t.index ["source_id"], name: "index_project_items_on_source_id", using: :btree
+    t.index ["status"], name: "index_project_items_on_status", using: :btree
     t.index ["tenant_id"], name: "index_project_items_on_tenant_id", using: :btree
     t.index ["user_id"], name: "index_project_items_on_user_id", using: :btree
   end
@@ -125,9 +128,9 @@ ActiveRecord::Schema.define(version: 20160719025752) do
     t.integer  "user_id"
     t.integer  "project_id"
     t.integer  "tenant_id"
+    t.integer  "role",       default: 200
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
-    t.integer  "role",       default: 200
     t.index ["project_id"], name: "index_project_users_on_project_id", using: :btree
     t.index ["tenant_id"], name: "index_project_users_on_tenant_id", using: :btree
     t.index ["user_id"], name: "index_project_users_on_user_id", using: :btree
@@ -137,11 +140,11 @@ ActiveRecord::Schema.define(version: 20160719025752) do
     t.string   "name"
     t.string   "description"
     t.integer  "user_id"
-    t.string   "status",      default: "100"
+    t.integer  "status",      default: 100
     t.integer  "tenant_id"
     t.string   "remark"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.index ["tenant_id"], name: "index_projects_on_tenant_id", using: :btree
     t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end

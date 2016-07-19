20d69928357c00d4f7013b4ff41af0b6d0abbb47
Charlot <7wangsong@gmail.com>
2016-07-19 17:11:59 +0800

    add unit string

--- app/models/kpi/cycle_time.rb
+++ app/models/kpi/cycle_time.rb
@@ -3,7 +3,7 @@ module Kpi
     default_scope { where(code: 'CYCLE_TIME') }
 
     def build_setting(project_item)
-      Kpi::Setting.new(kpi_id: self.id, project_item_id: project_item.id, tenant_id: project_item.tenant_id, name: self.name)
+      Kpi::Setting.new(kpi_id: self.id, project_item_id: project_item.id, tenant_id: project_item.tenant_id, name: self.name,unit_string:self.unit_string)
     end
 
     def setting(project_item)

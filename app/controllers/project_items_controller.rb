804d0ed4dd01239524779c8e91f3ae68a587feb9
lzding <472339105@qq.com>
2016-07-26 16:18:13 +0800

    fix

--- app/controllers/project_items_controller.rb
+++ app/controllers/project_items_controller.rb
@@ -24,43 +24,42 @@ class ProjectItemsController < ApplicationController
   # POST /project_items
   # POST /project_items.json
   def create
-    # @project_item = ProjectItem.new(project_item_params)
-    #
-    # respond_to do |format|
-    #   if @project_item.save
-    #     format.html { redirect_to @project_item, notice: 'Project item was successfully created.' }
-    #     format.json { render :show, status: :created, location: @project_item }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @project_item.errors, status: :unprocessable_entity }
-    #   end
-    # end
-    if project=Project.find_by_id(params[:project_id])
-      source=project.project_items.last
-      project_item=project.project_items.new({
-                                       user: current_user,
-                                       tenant: current_user.tenant,
-                                       status: ProjectItemStatus::ON_GOING,
-                                       name: ProjectItem.generate_name,
-                                       source_id: source.id
-                                   })
-      layout=JSON.parse(source.diagram.layout,symbolize_names:true)
-      layout[:nodeDataArray].each_with_index do |n, index|
-        node=project_item.nodes.new({
-                                           type: n[:returnParams][:type],
-                                           name: n[:returnParams][:name],
-                                           code: n[:returnParams][:code],
-                                           is_selected: n[:returnParams][:is_selected],
-                                           uuid: n[:returnParams][:uuid],
-                                           devise_code: n[:returnParams][:devise_code],
-                                           node_set: project_item.node_set,
-                                           tenant: current_user.tenant
-                                       })
+    ProjectItem.transaction do
+      if project=Project.find_by_id(params[:project_id])
+        source=project.project_items.last
+        project_item=project.project_items.create({
+                                                      user: user,
+                                                      tenant: user.tenant,
+                                                      status: ProjectItemStatus::ON_GOING,
+                                                      name: ProjectItem.generate_name,
+                                                      source_id: source.id
+                                                  })
+
+        layout=JSON.parse(source.diagram.layout, symbolize_names: true)
+        layout[:nodeDataArray].each_with_index do |n, index|
+          node=Node.create({
+                               type: n[:returnParams][:type],
+                               name: n[:returnParams][:name],
+                               code: n[:returnParams][:code],
+                               is_selected: n[:returnParams][:is_selected],
+                               uuid: n[:returnParams][:uuid],
+                               devise_code: n[:returnParams][:devise_code],
+                               node_set: project_item.node_set,
+                               tenant: user.tenant
+                           })
+          puts "-----------------------------------------------------------------------------------"
+          puts "------------t#{n[:returnParams][:id]}-------- t#{node.id}----------------------"
+          puts "------------t#{n[:returnParams][:node_set_id]}--------t#{project_item.node_set.id}----------------------"
+          layout[:nodeDataArray][index][:returnParams][:id] = node.id
+          layout[:nodeDataArray][index][:returnParams][:node_set_id] = project_item.node_set.id
+          puts "----------------------------------------------------------------------------------------------"
+        end
+        puts layout.to_json
+        project_item.diagram.update_attributes({layout: layout})
+      else
 
       end
-
     end
-
   end
 
   # PATCH/PUT /project_items/1
@@ -88,13 +87,13 @@ class ProjectItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project_item
-      @project_item = ProjectItem.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project_item
+    @project_item = ProjectItem.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def project_item_params
-      params.require(:project_item).permit(:user_id, :tenant_id, :project_id, :rank, :status, :source_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def project_item_params
+    params.require(:project_item).permit(:user_id, :tenant_id, :project_id, :rank, :status, :source_id)
+  end
 end
--- app/models/project_item.rb
+++ app/models/project_item.rb
@@ -22,6 +22,50 @@ class ProjectItem < ApplicationRecord
     "LC#{(Time.now.to_f*1000).to_i}"
   end
 
+
+  def self.test params
+    ProjectItem.transaction do
+      user = User.find_by_email('admin@ci.com')
+      if project=Project.find_by_id(params[:project_id])
+        source=project.project_items.last
+        project_item=project.project_items.create({
+                                                      user: user,
+                                                      tenant: user.tenant,
+                                                      status: ProjectItemStatus::ON_GOING,
+                                                      name: ProjectItem.generate_name,
+                                                      source_id: source.id
+                                                  })
+        puts '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+        puts source.diagram.layout
+
+
+        layout=JSON.parse(source.diagram.layout, symbolize_names: true)
+        layout[:nodeDataArray].each_with_index do |n, index|
+          node=Node.create({
+                               type: n[:returnParams][:type],
+                               name: n[:returnParams][:name],
+                               code: n[:returnParams][:code],
+                               is_selected: n[:returnParams][:is_selected],
+                               uuid: n[:returnParams][:uuid],
+                               devise_code: n[:returnParams][:devise_code],
+                               node_set: project_item.node_set,
+                               tenant: user.tenant
+                           })
+          puts "-----------------------------------------------------------------------------------"
+          puts "------------t#{n[:returnParams][:id]}-------- t#{node.id}----------------------"
+          puts "------------t#{n[:returnParams][:node_set_id]}--------t#{project_item.node_set.id}----------------------"
+          layout[:nodeDataArray][index][:returnParams][:id] = node.id
+          layout[:nodeDataArray][index][:returnParams][:node_set_id] = project_item.node_set.id
+          puts "----------------------------------------------------------------------------------------------"
+        end
+        puts layout.to_json
+        project_item.diagram.update_attributes({layout: layout})
+
+      end
+    end
+  end
+
+
   private
 
   def init_diagram

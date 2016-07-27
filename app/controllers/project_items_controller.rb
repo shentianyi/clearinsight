c55ace48267a4746d62baee79a898e7c1d07a31e
lzding <472339105@qq.com>
2016-07-27 15:59:33 +0800

    fix

--- app/controllers/project_items_controller.rb
+++ app/controllers/project_items_controller.rb
@@ -12,54 +12,6 @@ class ProjectItemsController < ApplicationController
   def show
   end
 
-  #
-  def improve
-    ProjectItem.transaction do
-      if source=ProjectItem.where(id: params[:id], status: ProjectItemStatus::FINISHED).first
-      # if source=ProjectItem.where(id: params[:id]).first
-        project=source.project
-        @project_item=project.project_items.create({
-                                                       user: current_user,
-                                                       tenant: current_user.tenant,
-                                                       status: ProjectItemStatus::ON_GOING,
-                                                       name: ProjectItem.generate_name,
-                                                       source_id: source.id
-                                                   })
-        # pdca_result=project_item.create_pdca params[:pdca], current_user
-        # if !pdca_result[:result]
-        #   return render :json => {result: false, content: pdca_result.content}
-        # end
-
-        if source.diagram.layout
-          layout=JSON.parse(source.diagram.layout, symbolize_names: true)
-          layout[:nodeDataArray].each_with_index do |n, index|
-            if source_node=Node.find_by_id(n[:key])
-              node=Node.create({
-                                   type: source_node.type,
-                                   name: source_node.name,
-                                   code: source_node.code,
-                                   is_selected: source_node.is_selected,
-                                   uuid: source_node.uuid,
-                                   devise_code: source_node.devise_code,
-                                   node_set: @project_item.node_set,
-                                   tenant: current_user.tenant
-                               })
-              layout[:nodeDataArray][index][:key] = node.id
-            end
-          end
-          puts layout.to_json
-          @project_item.diagram.update_attributes({layout: layout.to_json})
-        end
-
-        # redirect_to @project_item, notice: 'Project item was successfully updated.'
-        # render :json => {result: true, project_item: project_item, content: 'succ'}
-      else
-        render :json => {result: false, content: '轮次没有找到，或者状态未结束'}
-      end
-    end
-  end
-
-
   # GET /project_items/new
   def new
     @project_item = ProjectItem.new
@@ -73,42 +25,41 @@ class ProjectItemsController < ApplicationController
   # POST /project_items.json
   def create
     ProjectItem.transaction do
-      if project=Project.find_by_id(params[:project_id])
-        source=project.project_items.last
-        @project_item=project.project_items.create({
-                                                       user: user,
-                                                       tenant: user.tenant,
+      # if source=ProjectItem.where(id: params[:id], status: ProjectItemStatus::FINISHED).first
+      if source=ProjectItem.find_by_id(params[:project_item_id])
+        project=source.project
+        project_item=project.project_items.create({
+                                                       user: current_user,
+                                                       tenant: current_user.tenant,
                                                        status: ProjectItemStatus::ON_GOING,
                                                        name: ProjectItem.generate_name,
                                                        source_id: source.id
                                                    })
-        # pdca_result=project_item.create_pdca params[:pdca], current_user
-        # if !pdca_result[:result]
-        #   return render :json => {result: false, content: pdca_result.content}
-        # end
 
-        layout=JSON.parse(source.diagram.layout, symbolize_names: true)
-        layout[:nodeDataArray].each_with_index do |n, index|
-          if source_node=Node.find_by_id(n[:key])
-            node=Node.create({
-                                 type: source_node.type,
-                                 name: source_node.name,
-                                 code: source_node.code,
-                                 is_selected: source_node.is_selected,
-                                 uuid: source_node.uuid,
-                                 devise_code: source_node.devise_code,
-                                 node_set: @project_item.node_set,
-                                 tenant: user.tenant
-                             })
-            layout[:nodeDataArray][index][:key] = node.id
+        if source.diagram.layout
+          layout=JSON.parse(source.diagram.layout, symbolize_names: true)
+          layout[:nodeDataArray].each_with_index do |n, index|
+            if source_node=Node.find_by_id(n[:key])
+              node=Node.create({
+                                   type: source_node.type,
+                                   name: source_node.name,
+                                   code: source_node.code,
+                                   is_selected: source_node.is_selected,
+                                   uuid: source_node.uuid,
+                                   devise_code: source_node.devise_code,
+                                   node_set: project_item.node_set,
+                                   tenant: current_user.tenant
+                               })
+              layout[:nodeDataArray][index][:key] = node.id
+            end
           end
+          puts layout.to_json
+          project_item.diagram.update_attributes({layout: layout.to_json})
         end
-        puts layout.to_json
-        @project_item.diagram.update_attributes({layout: layout.to_json})
 
-        render :json => {result: true, project_item: @project_item, content: 'succ'}
+        render :json => {result: true, project_item: project_item, diagram: project_item.diagram, nodes: project_item.nodes, content: 'succ'}
       else
-        render :json => {result: false, content: 'Project没有找到'}
+        render :json => {result: false, content: '轮次没有找到'}
       end
     end
   end
@@ -117,7 +68,7 @@ class ProjectItemsController < ApplicationController
     if @project_item.blank? || (nodes = @project_item.nodes).blank?
       render :json => {result: false, content: '轮次没有找到'}
     else
-      render :json => {result: true, project_item: @project_item, nodes: @project_item.nodes, content: 'succ'}
+      render :json => {result: true, project_item: @project_item, nodes: @project_item.nodes, diagram: @project_item.diagram, content: 'succ'}
     end
   end
 

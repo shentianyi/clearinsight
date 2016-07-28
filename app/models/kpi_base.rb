158ff50d5923cc52e907fb3dc2e291bc2da91f42
Charlot <7wangsong@gmail.com>
2016-07-28 11:49:42 +0800

    add capacity setting

--- app/models/kpi_base.rb
+++ app/models/kpi_base.rb
@@ -2,34 +2,37 @@ class KpiBase < ApplicationRecord
   self.table_name='kpis'
 
   def self.create_settings(project_item)
-    Kpi::CycleTime.first.build_setting(project_item).save
-    Kpi::HumanCapacity.first.build_setting(project_item).save
+    # Kpi::CycleTime.first.build_setting(project_item).save
+    # Kpi::HumanCapacity.first.build_setting(project_item).save
+    [Kpi::CycleTime, Kpi::Capacity, Kpi::HumanCapacity].each do |kc|
+      kc.first.build_setting(project_item).save
+    end
+
   end
 
   def self.destroy_settings(project_item)
-    if setting= Kpi::CycleTime.first.setting(project_item)
-      setting.destroy
-    end
-    if setting= Kpi::HumanCapacity.first.setting(project_item)
-      setting.destroy
+    [Kpi::CycleTime, Kpi::Capacity, Kpi::HumanCapacity].each do |kc|
+      if setting= kc.first.setting(project_item)
+        setting.destroy
+      end
     end
   end
 
 
   def self.settings(project_item)
     settings={}
-    ct=Kpi::CycleTime.first
-    settings[ct.code] =ct.setting(project_item)
 
-    hc=Kpi::HumanCapacity.first
-    settings[hc.code]=hc.setting(project_item)
+    [Kpi::CycleTime, Kpi::Capacity, Kpi::HumanCapacity].each do |kc|
+       k=kc.first
+       settings[k.code] =k.setting(project_item)
+    end
 
     settings
   end
 
 
   def build_setting(project_item)
-    Kpi::Setting.new(kpi_id: self.id, project_item_id: project_item.id, tenant_id: project_item.tenant_id, name: self.name,unit_string:self.unit_string)
+    Kpi::Setting.new(kpi_id: self.id, project_item_id: project_item.id, tenant_id: project_item.tenant_id, name: self.name, unit_string: self.unit_string)
   end
 
   def setting(project_item)

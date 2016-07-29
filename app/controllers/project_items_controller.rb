0e47044ac36948ec3ee655604db4ab1baf48a1f9
lzding <472339105@qq.com>
2016-07-29 12:11:16 +0800

    fix

--- app/controllers/project_items_controller.rb
+++ app/controllers/project_items_controller.rb
@@ -75,7 +75,14 @@ class ProjectItemsController < ApplicationController
     if @project_item.blank?# || (nodes = @project_item.nodes).blank?
       render :json => {result: false, content: '轮次没有找到'}
     else
-      render :json => {result: true, project_item: @project_item, nodes: @project_item.nodes, diagram: @project_item.diagram, content: 'succ'}
+      render :json => {
+                 result: true,
+                 project_item: @project_item,
+                 nodes: @project_item.nodes,
+                 diagram: @project_item.diagram,
+                 settings: @project_item.kpi_settings,
+                 content: 'succ'
+             }
     end
   end
 

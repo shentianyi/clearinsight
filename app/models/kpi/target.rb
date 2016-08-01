a3a7673d9cc2ae555685265aa3574911579de0d6
Charlot <7wangsong@gmail.com>
2016-08-01 14:46:44 +0800

    c

--- app/models/kpi/target.rb
+++ app/models/kpi/target.rb
@@ -10,6 +10,8 @@ module Kpi
     field :code, type: String
     field :is_system, type: Boolean, default: false
 
+    validates_length_of :name, minimum: 1, maximum: 255,message:'名称长度需在1~255范围内'
+    validates_numericality_of :value,message:'值必须是数字'
     #belongs_to :setting, class_name: 'Kpi::Setting'
     embedded_in :setting, class_name: 'Kpi::Setting'
 
